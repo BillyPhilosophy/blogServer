27597463a9e6cab3dd39f29781ea9ab7917d4037
BillyPhilosophy <d497717947@gmail.com>
2022-09-28 22:46:21 +0800

    解析body添加服务层处理数据库

--- src/app/index.js
+++ src/app/index.js
@@ -1,6 +1,8 @@
 const Koa = require('koa');
+const koaBody = require('koa-body');
 const app = new Koa();
 const userRoute = require('../router/users.route');
 
+app.use(koaBody());
 app.use(userRoute.routes());
 module.exports = app;
--- src/controller/user.controller.js
+++ src/controller/user.controller.js
@@ -1,6 +1,15 @@
+const {createUser} = require('../service/users.service');
+
 class UserController {
   async register(ctx, next) {
-    ctx.body = "用户注册！";
+    // 1.读取请求体
+    // ctx.body = `Request Body: ${JSON.stringify(ctx.request.body)}`;
+    // 2.写入数据库
+    // todo:
+    const {user_name,password} = ctx.request.body;
+    const res = await createUser(user_name,password)
+    // 3.返回
+    ctx.body = res;
   }
   async login(ctx, next) {
     ctx.body = "登陆成功";
